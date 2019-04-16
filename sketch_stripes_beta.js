a8554929f65f99da4a811884310c8b0f39cb4490
Kiel Mutschelknaus <kieldm@gmail.com>
2019-04-15 22:18:55 -0400

    Update sketch_stripes_beta.js

--- sketch_stripes_beta.js
+++ sketch_stripes_beta.js
@@ -54,7 +54,7 @@ function preload() {
 }
 
 function setup() {
-  var p5SaveCanvas = createCanvas(540, 540);
+  var p5SaveCanvas = createCanvas(400, 600);
 //  createCanvas(windowWidth, windowHeight);
   canvas = p5SaveCanvas.canvas;
     
