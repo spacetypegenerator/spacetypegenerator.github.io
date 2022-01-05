41bf2d9fd84b95398f592df0fd8effe464276f45
Kiel M <43862107+spacetypegenerator@users.noreply.github.com>
2022-01-05 06:53:55 -0500

    Brain 2 launch

--- client/sketch_brain.js
+++ client/sketch_brain.js
@@ -1,29 +1,38 @@
 var pg = [];
+var pgPlus = [];
 
 var bkgdColor;
 
 var x = [];
 var xTarget = [];
+var xHold = [];
 var y = [];
 var yTarget = [];
+var yHold = [];
 var z = [];
 var zTarget = [];
+var zHold = [];
+var label = []
 
-var animOn = [];
+var a = [];
+var aTarget = [];
+var aHold = [];
 
-var animLength = 45;
-var animStart;
-var animStop;
-var returnLength = 20;
-var returnStart;
-var returnStop;
+var xNudge = [];
+var yNudge = [];
+var label = []
 
-var runReturn = false;
-var stateChanged = false;
+var animOn = false;
 
-var currentState = 0;
+var animLength = 30;
+var animStart = 0;
+var animStop = 0;
+
+var currentState = 6;
 
 var font1;
+
+var cullSpace = 0.1;
 
 function preload(){
   pg[0] = loadImage("resources/base.png");
@@ -32,11 +41,17 @@ function preload(){
   pg[3] = loadImage("resources/occipital2.png");
   pg[4] = loadImage("resources/temporal2.png");
 
-  font1 = loadFont("resources/Linotype - Neue Haas Grotesk Text Std 55 Roman.ttf");
+  pgPlus[1] = loadImage("resources/frontal2 plus.png");
+  pgPlus[2] = loadImage("resources/parietal2 plus.png");
+  pgPlus[3] = loadImage("resources/occipital2 plus.png");
+  pgPlus[4] = loadImage("resources/temporal2 plus.png");
+
+  font1 = loadFont("resources/SpaceMono-Bold.ttf");
 }
 
 function setup(){
-  createCanvas(1000,800,WEBGL);
+  let renderer = createCanvas(1000,800,WEBGL);
+  renderer.drawingContext.disable(renderer.drawingContext.DEPTH_TEST);
 
   bkgdColor = color("#ffffff");
   foreColor = color("#000000");
@@ -46,41 +61,65 @@ function setup(){
   for(var k = 0; k<5; k++){
     x[k] = 0;
     xTarget[k] = 0;
+    xHold[k] = 0;
     y[k] = 0;
     yTarget[k] = 0;
+    yHold[k] = 0;
     z[k] = 0;
     zTarget[k] = 0;
+    zHold[k] = 0;
+    a[k] = 0;
+    aTarget[k] = 0;
+    aHold[k] = 0;
 
     animOn[k] = false;
   }
+
+  xNudge[1] = -220;  yNudge[1] = -100;
+  xNudge[2] = 115;  yNudge[2] = -175;
+  xNudge[3] = 310;  yNudge[3] = 0;
+  xNudge[4] = 20;  yNudge[4] = 60;
+
+  label[1] = "Frontal";
+  label[2] = "Parietal";
+  label[3] = "Occipital";
+  label[4] = "Temporal";
 }
 
 function draw(){
   background(bkgdColor);
-  orbitControl();
+  // orbitControl();
 
-  if(runReturn){
-    returner();
-  } else {
+  if(animOn){
     animater();
   }
 
+  textFont(font1);
+  textSize(20);
+
   push();
-    push();
-      translate(x[0], y[0], z[0] - 1);
-      image(pg[0],-width/2,-height/2);
+    translate(x[0], y[0], z[0]);
+    image(pg[0],-width/2,-height/2);
 
-      for(var k = 1; k<5; k++){
-        push();
-          translate(x[k], y[k], z[k]);
-          image(pg[k],-width/2,-height/2);
-        pop();
-      }
-    pop();
+    translate(0,0,1);
+
+    for(var k = 1; k<5; k++){
+      push();
+        translate(x[k], y[k], z[k] + k*cullSpace);
+
+        tint(255, a[k]);
+        image(pgPlus[k],-width/2,-height/2);
+
+        translate(0,0,1);
+        tint(255,255)
+        image(pg[k],-width/2,-height/2);
+
+        translate(xNudge[k], yNudge[k] + 62 - a[k]/4, 1);
+        noStroke(); fill(84,165,236,a[k]);
+        rect(0,0,120,30);
+        fill(255,a[k]);
+        text(label[k],5,25);
+      pop();
+    }
   pop();
-
-}
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight,WEBGL);
 }
--- client/update.js
+++ client/update.js
@@ -1,135 +1,132 @@
 function setState(state){
-  currentState = state;
-  if(stateChanged){
-    runReturn = true;
-    returnStart = frameCount;
-    returnStop = frameCount + returnLength;
-  }
-  if(runReturn == false){
-    assignState();
-  }
-}
-
-function assignState(){
   animStart = frameCount;
   animStop = frameCount + animLength;
 
-  for(var k = 0; k<5; k++){
-    x[k] = 0;
-    xTarget[k] = 0;
-    y[k] = 0;
-    yTarget[k] = 0;
-    z[k] = 0;
-    zTarget[k] = 0;
-
-    animOn[k] = false;
+  if(state == currentState){
+    for(var k = 0; k<5; k++){
+      resetState(k);
+    }
+  } else {
+    currentState = state;
+    assignState();
   }
+  animOn = true;
+}
+
+function assignState(){
+  zTarget[0] = -300;
 
   if(currentState == 0){
-    animOn[0] = true;
-    zTarget[0] = -50;
+    xTarget[0] = 110;
+    yTarget[0] = 110;
 
-    animOn[1] = true;
-    xTarget[1] = -50;
-    yTarget[1] = -50;
+    xTarget[1] = -120;
+    yTarget[1] = -80;
+    zTarget[1] = 300;
+    aTarget[1] = 255;
 
-    animOn[2] = true;
-    xTarget[2] = 10;
-    yTarget[2] = -50;
+    xTarget[2] = 30;
+    yTarget[2] = -110;
+    zTarget[2] = 300;
+    aTarget[2] = 255;
+
+    resetState(3);
+    resetState(4);
   } else if(currentState == 1){
-    animOn[0] = true;
-    zTarget[0] = -50;
+    xTarget[0] = -100;
+    yTarget[0] = -20;
 
-    animOn[2] = true;
-    xTarget[2] = 10;
-    yTarget[2] = -50;
+    xTarget[2] = 150;
+    yTarget[2] = -60;
+    zTarget[2] = 300;
+    aTarget[2] = 255;
 
-    animOn[4] = true;
-    xTarget[4] = -20;
-    yTarget[4] = 50;
+    xTarget[4] = 40;
+    yTarget[4] = 160;
+    zTarget[4] = 300;
+    aTarget[4] = 255;
+
+    resetState(1);
+    resetState(3);
   } else if(currentState == 2){
-    animOn[0] = true;
-    zTarget[0] = -50;
+    xTarget[0] = -40;
+    yTarget[0] = 0;
 
-    animOn[3] = true;
     xTarget[3] = 50;
     yTarget[3] = 0;
+    zTarget[3] = 350;
+    aTarget[3] = 255;
+
+    resetState(1);
+    resetState(2);
+    resetState(4);
   } else if(currentState == 3){
-    animOn[0] = true;
-    zTarget[0] = -50;
+    xTarget[0] = -200;
+    yTarget[0] = 0;
 
-    animOn[2] = true;
-    xTarget[2] = 10;
-    yTarget[2] = -50;
+    xTarget[2] = 160;
+    yTarget[2] = -80;
+    zTarget[2] = 300;
+    aTarget[2] = 255;
 
-    animOn[3] = true;
-    xTarget[3] = 50;
+    xTarget[3] = 200;
     yTarget[3] = 0;
+    zTarget[3] = 300;
+    aTarget[3] = 255;
 
-    animOn[4] = true;
-    xTarget[4] = -20;
-    yTarget[4] = 50;
+    xTarget[4] = 120;
+    yTarget[4] = 100;
+    zTarget[4] = 300;
+    aTarget[4] = 255;
+
+    resetState(1);
   } else if(currentState == 4){
-    animOn[0] = true;
-    zTarget[0] = -50;
+    xTarget[0] = 130;
+    yTarget[0] = 60;
 
-    animOn[1] = true;
-    xTarget[1] = -50;
+    xTarget[1] = -100;
     yTarget[1] = -50;
+    zTarget[1] = 300;
+    aTarget[1] = 255;
+
+    resetState(2);
+    resetState(3);
+    resetState(4);
   }
 }
 
 function animater(){
-  // textFont(font1); fill(foreColor);
-  // text("ANIMATING!", width/2 - 100, height/2 - 100);
-
   for(var k = 0; k<5; k++){
-    if(animOn[k]){
-      z[0] = map(aSet(ticker, 5), 0, 1, 0, zTarget[0]);
+    var ticker = map(frameCount, animStart, animStop, 0, 1);
 
-      var ticker = map(frameCount, animStart, animStop, 0, 1);
+    x[k] = map(aSet(ticker, 5), 0, 1, xHold[k], xTarget[k]);
+    y[k] = map(aSet(ticker, 5), 0, 1, yHold[k], yTarget[k]);
+    z[k] = map(aSet(ticker, 5), 0, 1, zHold[k], zTarget[k]);
 
-      x[k] = map(aSet(ticker, 5), 0, 1, 0, xTarget[k]);
-      y[k] = map(aSet(ticker, 5), 0, 1, 0, yTarget[k]);
-      z[k] = map(aSet(ticker, 5), 0, 1, 0, zTarget[k]);
-    }
+    a[k] = map(aSet(ticker, 5), 0, 1, aHold[k], aTarget[k]);
+
     if(frameCount == animStop-1){
-      animOn[k] = false;
+      xHold[k] = xTarget[k];
+      yHold[k] = yTarget[k];
+      zHold[k] = zTarget[k];
+      aHold[k] = aTarget[k];
     }
   }
 
   if(frameCount == animStop-1){
-    stateChanged = true;
+    animOn = false;
   }
 }
 
-function returner(){
-  // textFont(font1); fill(foreColor);
-  // text("RETURNING!", width/2 - 100, height/2 - 100);
-
-  for(var k = 0; k<5; k++){
-
-    var ticker = map(frameCount, returnStart, returnStop, 0.5, 1);
-
-    x[k] = map(aSet(ticker, 4), 0.5, 1, xTarget[k], 0);
-    y[k] = map(aSet(ticker, 4), 0.5, 1, yTarget[k], 0);
-    z[k] = map(aSet(ticker, 4), 0.5, 1, zTarget[k], 0);
-  }
-
-  if(frameCount == returnStop-1){
-    runReturn = false;
-    stateChanged = false;
-    assignState();
-  }
-}
-
-function checkReturn(){
-
+function resetState(st){
+  xTarget[st] = 0;
+  yTarget[st] = 0;
+  zTarget[st] = 0;
+  aTarget[st] = 0;
 }
 
 function aSet(ticker, influ){          // takes a 0 - 1 and returns an eased 0 - 1
   var capTicker = ticker%1;
-
   var targetPoint = pow(capTicker,influ)/(pow(capTicker,influ) + pow(1-capTicker,influ));
   return targetPoint;
 }
